851e04fbdf25953e1bb6d88c8af1fee8b41a36ae
Sakibur Rahman Sabbir <106433632+gmsakibursabbir@users.noreply.github.com>
2023-10-02 22:34:53 +0600

    main

--- main.js
+++ main.js
@@ -6,11 +6,9 @@
    -> String basic <-
 ===*/
 
-
-
 // String surrounded by ('')single, ("")double quotes and (``)backticks.
 
-const single = "Single quotes";
+const single = 'Single quotes';
 const double = "Double quotes";
 
 // template literal
@@ -19,9 +17,6 @@ const backtick = `Backticks`;
 console.log(single);
 console.log(double);
 console.log(backtick);
-
-
-
 
 /*===
    -> Embeding JS <-
@@ -34,15 +29,12 @@ const name = "Chris";
 const greeting = `helo, ${name}`;
 console.log(greeting);
 
-
 // example with two variable
 const name1 = "Sakibur";
 const name2 = "Rahman";
 
 const joined = `${name1} ${name2} sabbir`;
 console.log(joined);
-
-
 
 /*===
    -> Concatenation in context <-
@@ -51,12 +43,43 @@ console.log(joined);
 const button = document.querySelector("button");
 
 function greet() {
-   const nameHi = prompt("What's your name?");
-   const greeting = document.querySelector("#sayhi");
-   greeting.textContent= `Hello ${nameHi}, Nice to meet you!`;
-};
+  const nameHi = prompt("What's your name?");
+  const greeting = document.querySelector("#sayhi");
+  greeting.textContent = `Hello ${nameHi}, Nice to meet you!`;
+}
 
 button.addEventListener("click", greet);
 
+/*===
+   -> Concatenation<-
+===*/
 
+// adding normal strings with the (+) operator.
 
+const greeting3 = "Hello";
+const name3 = "Sabbir";
+
+console.log(greeting3 + ", " + name3);
+
+console.log(`${greeting3}, ${name3}`);
+
+// Including expressions in strings
+
+const song = "E-Hawa";
+const score = 9;
+const highestScore = 10;
+const outputSong = `I like the song "${song}". I gave it a score of ${
+  (score / highestScore) * 100
+}%.`;
+
+console.log(outputSong);
+
+//note: to use backticks just use use backslash like this - \` ___ \`
+
+/*===
+   -> Multiline strings <-
+===*/
+
+const newline = `In order to get a multi-line string with the old style, we had to concatenate two strings with + sign and include a newline character. Writing multi-line strings became easier with template literals because you just need to add a new line in the string by pressing the enter key and type the text as you want it to appear.`;
+
+console.log(newline);
